493677ab383b126f01b75755bba61a73bbb4d147
jamil <164529574+VazirJamilCodeCraft@users.noreply.github.com>
2025-05-22 22:18:11 +0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,5 +1,5 @@
-public class Main {
+public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World from Jenkins!");
+        System.out.println("Hello from DevOps Jenkins pipeline!");
     }
 }
